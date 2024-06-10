e96e961a8dcee4d51d6b8259c553419f785c2871
Grigory Rechistov <grigory.rechistov@intel.com>
2024-06-10 09:46:34 +0200

    Use different syntax for initializationwq

--- ia32/ggg-cpuid.c
+++ ia32/ggg-cpuid.c
@@ -62,8 +62,7 @@ static void print_subleaf(uint32_t leaf, uint32_t subleaf, cpuid_result r) {
 static void cpuid_leaf(uint32_t leaf) {
     int subleaf = 0;
 
-    cpuid_result last_subleaf;
-    memset(&last_subleaf, 0, sizeof(last_subleaf));
+    cpuid_result last_subleaf = {0};
 
     for (subleaf = 0; subleaf > -1; ++subleaf) {
         cpuid_result r = do_cpuid(leaf, subleaf);
